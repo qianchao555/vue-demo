3076443138a6c1cb7f8cc6404e75ba417a2cb090
qianchao555 <xiaoyigedl@gmail.com>
2020-08-07 17:50:35 +0800

    0807提交

---
+++ src/components/Router1.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="router1">
+    <p>hello router1!</p>
+    <div v-for="site in info">
+      {{site.name}}
+    </div>
+
+
+    <router-link to="/foo">go to Foo</router-link>
+    <router-link to="/bar">go to bar</router-link>
+    <!--      路由出口，路由匹配到的组件将渲染在这里-->
+    <router-view></router-view>
+
+  </div>
+</template>
+
+
+<script type="text/javascript">
+  import axios from 'axios'
+
+  export default {
+    name: 'Router1',
+    data () {
+      return {
+        info: null,
+      }
+    },
+    mounted () {
+      axios
+        .get('https://www.runoob.com/try/ajax/json_demo.json')
+        .then(response => (this.info = response.data.sites))
+        .catch(function (error) { // 请求失败处理
+          console.log(error);
+        });
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/VueModel.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="vueModel">
+    <Runoob></Runoob>
+
+
+
+    <p>textarea 元素：</p>
+    <p style="white-space: pre">{{message2}}</p>
+    <textarea v-model="message2" placeholder="多行文本输入.."></textarea>
+
+    <p>单个复选框：</p>
+    <input type="checkbox" id="checkbox" v-model="checked">
+    <label for="checkbox" >{{checked}}</label><br>
+    <span>选择的值为：{{checked}}</span>
+
+    <p>多个复选框：</p>
+    <input type="checkbox" id="runoob" value="Runoob12" v-model="checkedNames">
+    <label for="runoob">Runoob</label>
+
+    <input type="checkbox" id="google" value="Google" v-model="checkedNames">
+    <label for="google">Google</label>
+
+    <input type="checkbox" id="taobao" value="Taobao" v-model="checkedNames">
+    <label for="taobao">taobao</label>
+    <br>
+    <span>选择的值为: {{ checkedNames }}</span>
+
+    <p>select列表：</p>
+    <select v-model="selected" name="fruit">
+      <option value="">选择一个网站</option>
+      <option value="www.runoob.com">Runoob</option>
+      <option value="www.google.com">Google</option>
+    </select>
+    <div id="output">
+      选择的网站是：{{selected}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'VueModel',
+    data () {
+      return {
+        message: 'qc',
+        message2: 'qc2',
+        checked : false,
+        checkedNames: [],
+        selected:''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
